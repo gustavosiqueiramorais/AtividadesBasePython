6b82bc7e43ee5385c5fcabc38a63a9ddc5592ace
Gustavo Siqueira <167713200+gustavosiqueiramorais@users.noreply.github.com>
2024-08-25 16:58:04 -0300

    Add files via upload

---
+++ Soma dois algarismos/SomaComPython.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um numero inteiro qualquer: '))
+n2 = int(input('Digite outro numero inteiro qualquer: '))
+soma = n1 + n2
+print('A soma entre esses dois numeros é igual a:',soma)
