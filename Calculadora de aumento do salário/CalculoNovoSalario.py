342fb11ad913760ba25b8e12f4559bc31d146161
Gustavo Siqueira <167713200+gustavosiqueiramorais@users.noreply.github.com>
2024-09-01 09:43:34 -0300

    Add files via upload

---
+++ Calculadora de aumento do salário/CalculoNovoSalario.py
@@ -0,0 +1,4 @@
+salario = int(input('Digite o valor do seu salario: '))
+calculo = salario * 0.15
+novoSalario = salario +  calculo
+print('O seu novo salario com 15 porcento de aumento será de {} reais'.format(novoSalario))
