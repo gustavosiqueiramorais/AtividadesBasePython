d63436b8259a3c5fc61d2d94c02a5b04f41f5d24
Gustavo Siqueira <167713200+gustavosiqueiramorais@users.noreply.github.com>
2025-02-27 16:58:25 -0300

    Delete Calculadora de Hipotenusa directory

--- Calculadora de Hipotenusa/CalculadoraHipotenusa.py
+++
@@ -1,9 +0,0 @@
-import math
-
-n1 = int(input('Digite o valor do cateto oposto: '))
-n2 = int(input('Digite o valor do cateto adjascente: '))
-
-calculo = n1**2 + n2**2 
-hipotenusa = math.sqrt(calculo)
-
-print('O valor da hipotenusa desse triangulo retangulo é de {}'.format(hipotenusa))
