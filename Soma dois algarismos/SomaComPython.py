4d626403683792f8b4e2a643a153d727560ea151
Gustavo Siqueira <167713200+gustavosiqueiramorais@users.noreply.github.com>
2025-02-27 16:58:58 -0300

    Delete Soma dois algarismos directory

--- Soma dois algarismos/SomaComPython.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('Digite um numero inteiro qualquer: '))
-n2 = int(input('Digite outro numero inteiro qualquer: '))
-soma = n1 + n2
-print('A soma entre esses dois numeros é igual a:',soma)
