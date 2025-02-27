746cb6cfbe340c6f8fad94c5bce57ded1ef2fe74
Gustavo Siqueira <167713200+gustavosiqueiramorais@users.noreply.github.com>
2025-02-27 16:58:34 -0300

    Delete Calculadora de aumento do salário directory

--- Calculadora de aumento do salário/CalculoNovoSalario.py
+++
@@ -1,4 +0,0 @@
-salario = int(input('Digite o valor do seu salario: '))
-calculo = salario * 0.15
-novoSalario = salario +  calculo
-print('O seu novo salario com 15 porcento de aumento será de {} reais'.format(novoSalario))
