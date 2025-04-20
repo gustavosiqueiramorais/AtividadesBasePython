99a4ab5a732b4e320613067d86b22ef152ea17db
Gustavo Siqueira <167713200+gustavosiqueiramorais@users.noreply.github.com>
2025-04-20 08:39:06 -0300

    Add files via upload

---
+++ Exercicio048/ex048.py
@@ -0,0 +1,6 @@
+soma = 0
+for c in range (1,500):
+    if c % 2 != 0 and c % 3 ==0:
+        soma = soma + c
+print(soma)
+print('Esta é a soma de todos os numeros impares multiplos de tres entre 1 e 500')
